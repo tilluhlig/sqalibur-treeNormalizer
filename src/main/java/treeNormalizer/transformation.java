4149d1b0bf7f80fe3ea23b4e6973380580fac5cb
Till Uhlig <till.uhlig@student.uni-halle.de>
2017-01-23 02:03:21 +0100

    Lizenz

--- src/main/java/treeNormalizer/transformation.java
+++ src/main/java/treeNormalizer/transformation.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2016 Till Uhlig <till.uhlig@student.uni-halle.de>
+/* 
+ * Copyright (C) 2017 Till Uhlig <till.uhlig@student.uni-halle.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
